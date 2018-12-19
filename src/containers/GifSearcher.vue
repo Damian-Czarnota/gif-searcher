0af5e5ddd3c719fe22707e4da2c613e321a37709
Damian-Czarnota <yziek@windowslive.com>
2018-12-19 10:14:18 +0100

    Adds function to add/remove favorite gif

--- src/containers/GifSearcher.vue
+++ src/containers/GifSearcher.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
     <Searcher @change-gif-name="getGifsComponent"/>
-    <h1>Results</h1>
-    <Grid :config="config" :data="gifsArray" @add-to-favorite="addToFavorites" @remove-from-favorite="removeFromFavorites"/>
-    <h1>Favorites</h1>
-    <Grid :config="config" :data="favoritesArray" @add-to-favorite="addToFavorites" @remove-from-favorite="removeFromFavorites"/>
+      <div class="header-center">
+          <h1>Results</h1>
+      </div>
+    <Grid v-if="gifsArray.length>0" :config="config" :data="gifsArray" @add-to-favorite="addToFavorites" @remove-from-favorite="removeFromFavorites"/>
+      <div v-if="gifsArray.length===0" class="empty-info">
+          <i class="far fa-images"></i>
+          <p>There is no data.</p>
+          <p>Start form searching or change parameters</p>
+      </div>
+      <div class="header-center">
+        <h1>Favorites</h1>
+    </div>
+    <Grid v-if="favoritesArray.length>0" :config="config" :data="favoritesArray" @add-to-favorite="addToFavorites" @remove-from-favorite="removeFromFavorites"/>
+      <div v-if="favoritesArray.length===0" class="empty-info">
+          <i class="fas fa-star"></i>
+          <p>There is no data.</p>
+          <p>Start adding gifs to favorites</p>
+      </div>
   </div>
 </template>
 
@@ -23,38 +37,52 @@ import Grid from '../components/Grid.vue';
 export default class GifSearcher extends Vue {
   private gifName: string = '';
   private gifsArray: Array<{}> = [];
-  private favoritesArray: List<{}> = [];
+  private favoritesArray: Array<{}> = [];
   private config: Array<{}> = [
   { key: 'image_url', title: 'Gif', type: 'image' },
   { key: 'bitly_gif_url', title: 'URL', type: 'code' },
   { key: 'title', title: 'Title', type: 'text' },
   { key: 'favorite', title: 'Favorite', type: 'favorite' },
   ];
+    constructor(){
+        super();
+        if(sessionStorage.getItem('favoritesArray'))
+            this.favoritesArray = JSON.parse(sessionStorage.getItem('favoritesArray'));
+    }
   private getGifsComponent(gifName: string) {
     DataService.getGifs(gifName).then(
             ({data}) => {
               this.gifsArray = data.data;
               this.gifsArray.forEach((item: any, index) => {
                 this.gifsArray[index].image_url = item.images.fixed_height.url;
-                this.gifsArray[index].favorite = false;
+                if(this.favoritesArray)
+                        this.favoritesArray.forEach(el =>{
+                            if(el.id===this.gifsArray[index].id)
+                                this.gifsArray[index].favorite = el.favorite;
+                        })
               });
             },
             (error) => {
                 alert('Something went wrong... Try again later');
             });
   }
-  //@TODO
-  private addToFavorites(item: object) {
+  private addToFavorites(item: any) {
       item.favorite=!item.favorite;
+      Vue.set(this.gifsArray,this.gifsArray.indexOf(item), {...item,favorite:item.favorite})
       this.favoritesArray.push(item);
+      sessionStorage.setItem('favoritesArray',JSON.stringify(this.favoritesArray));
   }
-
-  //@TODO
-  private removeFromFavorites(item: object) {
+  private removeFromFavorites(item: any) {
     item.favorite=!item.favorite;
+      this.gifsArray.forEach(el =>{
+          if(el.id===item.id)
+            el.favorite=item.favorite;
+      });
     this.favoritesArray = this.favoritesArray.filter((el: any) =>{
-              return el!=item;
+            if(el.id!=item.id)
+              return el;
             });
+    sessionStorage.setItem('favoritesArray',JSON.stringify(this.favoritesArray));
   }
 }
 </script>
